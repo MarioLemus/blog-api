a7c33b804d92f4ab8a9bcc57891f9f133583c7b1
MarioLemus <theliarnorland@gmail.com>
2024-05-19 00:33:40 -0600

    add user route

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
+import 'module-alias/register'
 import express from 'express'
 import cors from 'cors'
 import morgan from 'morgan'
+import userRoutes from '@routes/v1/usersRoutes'
 
 const server = express()
 const PORT = 4000
@@ -9,10 +11,7 @@ const LOGGER_OPT = 'dev'
 server.use(cors())
 server.use(morgan(LOGGER_OPT))
 server.use(express.json())
-
-server.get('/', (req, res) => {
-    res.send('prueba')
-})
+server.use('/api/v1', userRoutes)
 
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}!`)
