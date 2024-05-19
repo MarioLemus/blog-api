22f87930b84b31f0a59a0c7b66811bf4ccdbbad0
MarioLemus <theliarnorland@gmail.com>
2024-05-19 00:35:38 -0600

    refactor routes

---
+++ src/interfaces/index.ts
@@ -0,0 +1,15 @@
+import {ControllerCreateOne} from './IControllers/ControllerCreateOne'
+import {ControllerDeleteMany} from './IControllers/ControllerDeleteMany'
+import {ControllerDeleteOne} from './IControllers/ControllerDeleteOne'
+import {ControllerGetAll} from './IControllers/ControllerGetAll'
+import {ControllerGetOne} from './IControllers/ControllerGetOne'
+import {ControllerUpdateOne} from './IControllers/ControllerUpdateOne'
+
+export {
+    ControllerCreateOne,
+    ControllerDeleteMany,
+    ControllerDeleteOne,
+    ControllerGetAll,
+    ControllerGetOne,
+    ControllerUpdateOne
+}
