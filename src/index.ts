9163b2c3463fd982fd01d26ee8be4b133fddb756
MarioLemus <theliarnorland@gmail.com>
2024-07-14 17:38:16 -0600

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,48 @@
 import 'module-alias/register'
 import userRoutes from '@routes/v1/userRoute'
 import {connectDB} from '@utils/connectDB'
-//import {} from '@utils/se'
 import express from 'express'
 import cors from 'cors'
 import morgan from 'morgan'
 import config from 'config'
-import path from 'node:path'
-import dotenv from 'dotenv'
+import {AddressInfo} from 'net'
+import {config as envConfig} from 'dotenv'
 
-const environment = process.env.NODE_ENV || 'dev'
-const envPath = path.resolve(process.cwd(), 'env', `.env.${environment}`);
-// load env folder path
-dotenv.config({ path: envPath });
+envConfig()
+
+const PORT = config.get('server.port') as number
+const DB_URI = config.get('db.uri') as string
+const LOGGER_OPT = config.get('logger.dev') as string
+const ERROR_PORT_TAKEN = 'EADDRINUSE'
 
 const server = express()
-const PORT = config.get('server.port') as number
-const LOGGER_OPT = config.get('logger.dev') as string
+const allowedOrigins = ['http://localhost:5173']
 
-//connectDB('mongo')
+connectDB('mongo', DB_URI)
 
-server.use(cors())
+server.use(cors({origin: allowedOrigins}))
 server.use(morgan(LOGGER_OPT))
 server.use(express.json())
 server.use('/api/v1', userRoutes)
 
-server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}!`)
-})
+function startServer(port: number) {
+    const currServer = server.listen(port, () => {        
+        const serverInfo = currServer.address() as AddressInfo
+        console.log(
+            serverInfo.address === '::'
+            ? `Server running on http://localhost:${port}` 
+            : `Server running on ${serverInfo.address}`
+        )
+    })
+    
+    currServer.on('error', (err: NodeJS.ErrnoException) => {
+        if (err.code === ERROR_PORT_TAKEN && process.env.NODE_ENV === 'development') {
+            console.log(`Port ${port} is already in use. Trying port ${port + 1}`)
+            startServer(port + 1)
+            return
+        }
+        console.log('Error starting server')
+    })
+}
+
+startServer(PORT)
