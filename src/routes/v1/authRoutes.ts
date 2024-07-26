ae4e31a7aee355b0f5cb56e5a9c86534b937342b
MarioLemus <theliarnorland@gmail.com>
2024-07-25 20:57:00 -0600

    add auth routes

--- src/routes/v1/authRoutes.ts
+++ src/routes/v1/authRoutes.ts
@@ -5,9 +5,9 @@ const router = express.Router()
 const user = new UserController()
 
 router.post('/user', user.createOne)
-router.get('/user', user.getAll)
 router.get('/user/:id', user.getOne)
 router.put('/user/:id', user.updateOne)
 router.delete('/user/:id', user.deleteOne)
+router.get('/user', user.getAll)
 
 export default router
