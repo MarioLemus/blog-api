90cec941254873a59edaad4f6424a7bc37eb9491
MarioLemus <theliarnorland@gmail.com>
2024-07-25 20:52:50 -0600

    change interface order

--- src/interfaces/IControllers.ts
+++ src/interfaces/IControllers.ts
@@ -1,25 +1,26 @@
 import {Request, Response} from 'express'
 
 export interface ControllerCreateOne {
-    createOne(req?:Request, res?:Response): void;
-}
-
-export interface ControllerDeleteMany {
-    deleteMany(req?:Request, res?:Response): void;
+    createOne(req?:Request, res?:Response): Promise<void>;
 }
 
 export interface ControllerDeleteOne {
-    deleteOne(req?:Request, res?:Response): void;
+    deleteOne(req?:Request, res?:Response): Promise<void>;
 }
 
-export interface ControllerGetAll {
-    getAll(req?:Request, res?:Response): void;
-}
 
 export interface ControllerGetOne {
-    getOne(req?:Request, res?:Response): void;
+    getOne(req?:Request, res?:Response): Promise<void>;
 }
 
 export interface ControllerUpdateOne {
-    updateOne(req?:Request, res?:Response): void;
+    updateOne(req?:Request, res?:Response): Promise<void>;
+}
+
+export interface ControllerDeleteMany {
+    deleteMany(req?:Request, res?:Response): Promise<void>;
+}
+
+export interface ControllerGetAll {
+    getAll(req?:Request, res?:Response): Promise<void>;
 }
